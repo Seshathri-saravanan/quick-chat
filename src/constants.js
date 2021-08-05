b2cee63c516e77cfcb52bef8eb7c51561da6cbfe
seshathri S <seshathrisaravanan@gmail.com>
2021-08-05 15:12:06 +0530

    added config for local deployment

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-//const URL = "http://localhost:3030"
-const URL = "https://quick-chat-2021-server.herokuapp.com"
+const URL = "http://localhost:8888"
+//const URL = "https://quick-chat-2021-server.herokuapp.com"
 export default URL;
 //"https://quick-chat-2021-server.herokuapp.com"
 //"http://localhost:8080"
